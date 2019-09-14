9ef163698433f51c352fcfd029724d2ef21852fd
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-14 17:29:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,17 @@
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+    
+  elsif phrase == phrase.upcase
+  puts "NO, NOT SINCE 1938!"
+  
+else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+  
+  
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
